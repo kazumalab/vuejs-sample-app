c1e898228425ecb2df6657489276dd6c73c68ea2
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-04-09 20:54:46 +0900

    Add: service worker install

---
+++ sw.js
@@ -0,0 +1,15 @@
+var CACHE_NAME = 'vuejs-cache-v1';
+var urlsToCache = [
+  '/'
+];
+
+self.addEventListener('install', function(event) {
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
