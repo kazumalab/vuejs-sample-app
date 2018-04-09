bde7e65a921f2e21a4bbeab984a22010c6659b50
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-04-09 21:08:44 +0900

    using cache

--- sw.js
+++ sw.js
@@ -4,12 +4,24 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // インストール処理
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
